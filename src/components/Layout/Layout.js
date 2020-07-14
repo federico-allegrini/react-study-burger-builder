85d89d4668566c4c5eee6f59d270272425ea0e57
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-07-14 14:56:00 +0200

    Adding navigation items, adding responsive sidedrawer with backdrop, adding toggle hamburger button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,33 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
-const layout = (props) => (
-  <Fragment>
-    <Toolbar />
-    <main className={classes.Content}>{props.children}</main>
-  </Fragment>
-);
-export default layout;
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  siteDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  siteDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Toolbar drawerToggleClicked={this.siteDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.siteDrawerCloseHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Fragment>
+    );
+  }
+}
+export default Layout;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,8 @@
 import React from "react";
 import classes from "./Logo.module.css";
 import burgerLogo from "../../assets/images/burger-logo.png";
-const logo = () => (
-  <div className={classes.Logo}>
+const logo = (props) => (
+  <div className={classes.Logo} style={{ height: props.height }}>
     <img src={burgerLogo} alt="MyBurger"></img>
   </div>
 );
