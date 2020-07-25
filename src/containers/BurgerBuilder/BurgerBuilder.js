0fc806cd4dea35fe8ea5f74bb50faad66f6f3c23
Federico Allegrini <federico.allegrini.dev@gmail.com>
2020-07-25 16:50:31 +0200

    Building the checkout container, setting up routing and routes, navigating to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,29 +89,7 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Federico Allegrini",
-        address: {
-          street: "Test Street 1",
-          zipCode: "02352",
-          country: "Österreich",
-        },
-        email: "test@test.com",
-      },
-      deliveryMethod: "fastest",
-    };
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    this.props.history.push("/checkout");
   };
 
   render() {
