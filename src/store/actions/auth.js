de16f967a6378ad9246374073d24d29d6373c516
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-09-14 14:23:03 +0200

    Handling authentication eith a saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -45,36 +45,11 @@ export const checkAuthTimeout = (expirationTime) => {
 };
 
 export const auth = (email, password, isSignup) => {
-  return (dispatch) => {
-    dispatch(authStart());
-    const authData = {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    };
-    const apiKey = "AIzaSyDw6aB_glbeelWSjFanlL7_uBRylES0qOg";
-    let url =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + apiKey;
-    if (!isSignup) {
-      url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
-        apiKey;
-    }
-    axios
-      .post(url, authData)
-      .then((response) => {
-        const expirationDate = new Date(
-          new Date().getTime() + response.data.expiresIn * 1000
-        );
-        localStorage.setItem("token", response.data.idToken);
-        localStorage.setItem("expirationDate", expirationDate);
-        localStorage.setItem("userId", response.data.localId);
-        dispatch(authSuccess(response.data.idToken, response.data.localId));
-        dispatch(checkAuthTimeout(response.data.expiresIn));
-      })
-      .catch((error) => {
-        dispatch(authFail(error.response.data.error));
-      });
+  return {
+    type: actionTypes.AUTH_USER,
+    email: email,
+    password: password,
+    isSignup: isSignup,
   };
 };
 
