97dc66d1f3ed60a035f0b49abd2db238b8881be6
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-10-08 11:30:33 +0200

    Adding a custom error handling hook

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,39 +1,13 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment } from "react";
 import Modal from "../../components/UI/Modal/Modal";
+import useHttpErrorHandler from "../../hooks/http-error-handler";
+
 const withErrorHandler = (WrappedComponent, axios) => {
   return (props) => {
-    const [error, setError] = useState(null);
-
-    // componentWillMount() can be replaced with removing the function and leave the code alone
-    // The code will be executed before the render of component, as in componentWillMount()
-    const reqInterceptor = axios.interceptors.request.use((req) => {
-      setError(null);
-      return req;
-    });
-    const resInterceptor = axios.interceptors.response.use(
-      (res) => res,
-      (err) => {
-        setError(err);
-        return Promise.reject(err);
-      }
-    );
-
-    // componentWillUnmount() can be replaced with useEffect() return a function, with [] as second parameter
-    // In this case the second parameter has the two values because we want to clean up when req/res-interceptor changes
-    useEffect(() => {
-      return () => {
-        axios.interceptors.request.eject(reqInterceptor);
-        axios.interceptors.request.eject(resInterceptor);
-      };
-    }, [reqInterceptor, resInterceptor]);
-
-    const errorConfirmedHandler = () => {
-      setError(null);
-    };
-
+    const [error, clearError] = useHttpErrorHandler(axios);
     return (
       <Fragment>
-        <Modal show={error} modalClosed={errorConfirmedHandler}>
+        <Modal show={error} modalClosed={clearError}>
           {error ? error.message : null}
         </Modal>
         <WrappedComponent {...props} />
